5c440d4622cc80c803dbd24b9ea7aecfef52c24f
Shakti Prasad Patro <patro.s@husky.neu.edu>
2015-03-12 21:46:28 -0400

    Week 7 commit

---
+++ WebDevExperiments/experiments/js/week700.js
@@ -0,0 +1,41 @@
+﻿var app = angular.module("FormApp", []);
+
+
+app.controller("FormController", function myContoller($scope, $http) {
+
+    $scope.add = function (player) {
+
+        $http.post("http://nodejs-shakti200.rhcloud.com/api/player", player)
+           .success(function (response) {
+               $scope.added = true;
+           });
+    }
+
+    $scope.update = function (player) {
+        $http.put("http://nodejs-shakti200.rhcloud.com/api/player/" + player._id, player)
+        .success(function (response) {
+            $scope.updated = true;
+            $scope.uploaded = null;
+        });
+    };
+
+    $scope.upload = function () {
+        $http.get("http://nodejs-shakti200.rhcloud.com/api/player/1")
+        .success(function (response) {
+            $scope.player = response;
+            $scope.uploaded = true;
+        });
+        
+    }
+
+    $scope.delete = function (id) {
+        $http.delete("http://nodejs-shakti200.rhcloud.com/api/player/"+id)
+        .success(function (response) {
+            $scope.updated = true;
+            $scope.uploaded = null;
+            $scope.player = null;
+        });
+
+    }
+
+});
---
+++ nodejs/nodejs/public/js/app.js
@@ -0,0 +1,73 @@
+﻿var app = angular.module("CMApp", []);
+app.controller("BookController",
+    function ($scope, $http) {
+        $http.get("/api/book")
+        .success(function (response) {
+            $scope.booklist = response;
+        });
+
+        $scope.remove = function (index) {
+            $http.delete("/api/book/" + index)// to delete data from the client-side
+            .success(function (response) {
+                $scope.booklist = response; // update the list after a row has been deleted
+            });
+        };
+        $scope.add = function (book){ 
+            $http.post("/api/book", book) // to add data onto the client-side
+            .success(function (response) {
+                $scope.booklist = response;// update the list after a row has been added
+            });
+        };
+        $scope.selectedIndex = null;
+        $scope.select = function (index)
+        {
+            $scope.selectedIndex = index;
+            $scope.book = $scope.booklist[index];
+        }
+        $scope.update = function (book) {
+            $http.put("/api/book/" + $scope.selectedIndex, book) // update a particular row
+            .success(function (response) {
+                $scope.booklist = response;// update the list after a row has been updated
+            });
+
+        };
+
+    });
+
+
+app.controller("PlayerController",
+    function ($scope, $http) {
+        $http.get("/api/player")
+        .success(function (response) {
+            $scope.players = response;
+        });
+
+        $scope.remove = function (id) {
+            $http.delete("/api/player/" + id)// to delete data from the client-side
+            .success(function (response) {
+                $scope.players = response; // update the list after a row has been deleted
+            });
+        };
+        $scope.add = function (player) {
+            $http.post("/api/player", player) // to add data onto the client-side
+            .success(function (response) {
+                $scope.players = response;// update the list after a row has been added
+                $scope.player = null;
+            });
+        };
+        $scope.selectedIndex = null;
+        $scope.select = function (index) {
+            $scope.selectedIndex = index;
+            $scope.player = $scope.players[index];
+
+        }
+        $scope.update = function (player) {
+            $http.put("/api/player/" + player._id, player) // update a particular row
+            .success(function (response) {
+                $scope.players = response;// update the list after a row has been updated
+                $scope.player = null;
+            });
+
+        };
+
+    });
